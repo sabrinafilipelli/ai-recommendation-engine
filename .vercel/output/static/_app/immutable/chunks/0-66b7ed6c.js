fc8e81159ef53963d6cee39f6a549b47a11fec92
sabrinafilipelli <sabrinagear.ft@gmail.com>
2023-06-13 06:06:50 -0500

    fix

--- .vercel/output/static/_app/immutable/chunks/0-66b7ed6c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-f1385131.js";export{t as component};
--- .vercel/output/static/_app/immutable/chunks/2-a3b38fb9.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-2b2db097.js";export{t as component};
