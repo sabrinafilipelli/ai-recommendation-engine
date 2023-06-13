d3439bd236fff75d40e368b4fd3f6813ae55a36c
sabrinafilipelli <sabrinagear.ft@gmail.com>
2023-06-13 05:42:45 -0500

    import project files

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,11 @@
+<script>
+	import GitHubButton from './GitHubButton.svelte';
+</script>
+
+<div
+	class="max-w-4xl mx-auto w-full flex md:flex-row flex-col items-center md:justify-between py-8 mt-20 border-t border-white/20"
+>
+	<div class="text-white/70 flex items-center flex-none">
+		A Sabrina Filipelli project - powered by OpenAI
+	</div>
+</div>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+	import TvIcon from './TvIcon.svelte';
+</script>
+
+<div
+	class="max-w-4xl mx-auto w-full flex md:flex-row flex-col items-center md:justify-between py-8 mb-12 border-b border-white/20"
+>
+	<button on:click class="flex items-center mb-4 md:mb-0 text-white/70">
+		<TvIcon />
+		<div class="text-2xl md:text-xl font-bold ml-2">BingeWatcher.ai</div>
+	</button>
+</div>
